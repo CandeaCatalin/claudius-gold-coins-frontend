13cba02c3bdb00b8d6db853cab45862b6ca1e5a1
Cătălin Candea <80335024+CandeaCatalin@users.noreply.github.com>
2025-02-28 21:56:37 +0200

    Add Contact content

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent{
 
 }
